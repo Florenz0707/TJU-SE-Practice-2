33214820e7c320221c9f31da0bbd51d110628056
Florenz <florenz@tju.edu.cn>
2025-09-24 23:38:22 +0800

    [Nothing]

--- elm-v2.0/src/main/java/cn/edu/tju/elm/service/BusinessApplicationService.java
+++ elm-v2.0/src/main/java/cn/edu/tju/elm/service/BusinessApplicationService.java
@@ -33,8 +33,8 @@ public class BusinessApplicationService {
         return businessApplicationOptional.map(Utils::filterEntity).orElse(null);
     }
 
-    public BusinessApplication updateBusinessApplication(BusinessApplication businessApplication){
-        return businessApplicationRepository.save(businessApplication);
+    public void updateBusinessApplication(BusinessApplication businessApplication){
+        businessApplicationRepository.save(businessApplication);
     }
 
     public List<BusinessApplication> getBusinessApplicationsByApplicant(User applicant){
