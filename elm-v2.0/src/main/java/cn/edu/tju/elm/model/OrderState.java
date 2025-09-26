cf8acbf004ac3cb69666107578dcfc51e48111fb
Florenz <florenz@tju.edu.cn>
2025-09-26 12:49:04 +0800

    [Fix] Modify definition of OrderState, and the process of updateOrderState.

--- elm-v2.0/src/main/java/cn/edu/tju/elm/model/OrderState.java
+++ elm-v2.0/src/main/java/cn/edu/tju/elm/model/OrderState.java
@@ -2,10 +2,11 @@ package cn.edu.tju.elm.model;
 
 public class OrderState {
     public static final Integer CANCELED = 0;
-    public static final Integer UNPAID = 1;
-    public static final Integer DELIVERY = 2;
-    public static final Integer COMPLETE = 3;
-    public static final Integer COMMENTED = 4;
+    public static final Integer PAID = 1;
+    public static final Integer ACCEPTED = 2;
+    public static final Integer DELIVERY = 3;
+    public static final Integer COMPLETE = 4;
+    public static final Integer COMMENTED = 5;
 
     public static boolean isValidOrderState(Integer orderState) {
         return CANCELED <= orderState && orderState <= COMMENTED;
