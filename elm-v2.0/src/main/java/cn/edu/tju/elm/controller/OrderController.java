52e98676c8caf03656dad85f4413eddbf5bd78bf
Florenz <florenz@tju.edu.cn>
2025-09-26 13:07:45 +0800

    [Fix] Set new order PAID.

--- elm-v2.0/src/main/java/cn/edu/tju/elm/controller/OrderController.java
+++ elm-v2.0/src/main/java/cn/edu/tju/elm/controller/OrderController.java
@@ -78,7 +78,7 @@ public class OrderController {
 
             Utils.setNewEntity(order, me);
             order.setOrderTotal(totalPrice);
-            order.setOrderState(OrderState.COMPLETE);
+            order.setOrderState(OrderState.PAID);
             order.setOrderDate(order.getCreateTime());
             order.setBusiness(business);
             order.setCustomer(me);
